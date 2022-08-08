42e71ce6d378b535f989cf2876bc60afdfa54f47
Wellington Cruz <marcelinoc.wellington@gmail.com>
2022-08-08 20:56:38 -0300

    Apps desenvolvidos no Bootcamp Santander Mobile 2022.

---
+++ busca-repositoio-main/app/src/main/java/com/davideploy/repository/searcher/ui/RepoListAdapter.kt
@@ -0,0 +1,51 @@
+package com.davideploy.repository.searcher.ui
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.davideploy.repository.searcher.data.model.Repo
+import com.davideploy.repository.searcher.databinding.ItemRepoBinding
+import com.bumptech.glide.Glide
+
+class RepoListAdapter : ListAdapter<Repo, RepoListAdapter.ViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemRepoBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemRepoBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Repo) {
+            binding.tvRepoName.text = item.name
+            binding.tvRepoDescription.text = item.description
+            binding.tvRepoLanguage.text = item.language
+            binding.chipStar.text = item.stargazersCount.toString()
+
+            binding.widgetRepo.setOnClickListener {
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(item.htmlURL)
+                itemView.context.startActivity(i)
+            }
+
+            Glide.with(binding.root.context)
+                .load(item.owner.avatarURL).into(binding.ivOwner)
+        }
+    }
+}
+
+class DiffCallback : DiffUtil.ItemCallback<Repo>() {
+    override fun areItemsTheSame(oldItem: Repo, newItem: Repo) = oldItem == newItem
+    override fun areContentsTheSame(oldItem: Repo, newItem: Repo) = oldItem.id == newItem.id
+}
